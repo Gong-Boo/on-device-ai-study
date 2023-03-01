7231a9bcdaaa6681f13a34b8af0633179bbebe13
Jin Park <parkjin0318@naver.com>
2023-03-01 21:47:32 +0900

    add: MainView

--- 03.ML Kit 소개/parkjin/app/src/main/java/com/parkjin/mlkit/MainActivity.kt
+++ 03.ML Kit 소개/parkjin/app/src/main/java/com/parkjin/mlkit/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.parkjin.mlkit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val rootView = MainView(this)
+        setContentView(rootView)
     }
 }
---
+++ 03.ML Kit 소개/parkjin/app/src/main/java/com/parkjin/mlkit/MainView.kt
@@ -0,0 +1,47 @@
+package com.parkjin.mlkit
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.AttributeSet
+import android.widget.Button
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyle) {
+
+    private val button = Button(context).apply {
+        layoutParams = LayoutParams(
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.WRAP_CONTENT
+        )
+        text = context.getString(R.string.button_text)
+    }
+
+    private val image = ImageView(context).apply {
+        layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.MATCH_PARENT
+        )
+    }
+
+    init {
+        initializeView()
+    }
+
+    private fun initializeView() {
+        addView(button)
+        addView(image)
+    }
+
+    fun setOnButtonClickListener(block: () -> Unit) {
+        button.setOnClickListener { block() }
+    }
+
+    fun setImageBitmap(bitmap: Bitmap) {
+        image.setImageBitmap(bitmap)
+    }
+}
